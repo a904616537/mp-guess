49cd580430fa25d3b16c24e91f791f01fe5ac99e
Kain Shi <a904616537@outlook.com>
2018-07-05 14:16:13 +0800

    mp-vue init

---
+++ src/store/mutation-types.js
@@ -0,0 +1,27 @@
+/**
+# Sample Wechat MP
+* @Author: Sumi <xumin.1991@163.com>
+* @DateTime:    2018-06-14 16:15:52
+* @Description
+* @Flow 
+*/
+
+export const DEVRENENT = 'counter/decrement'
+export const INCREMENT = 'counter/increment'
+
+export const HALL_PRODUCT = 'hall/product'
+
+export const LOGIN         = 'user/login'
+export const SETSESSIONKEY = 'user/session_key'
+export const USERDETAIL    = 'user/detail'
+export const USER_PLAY     = 'user/play'
+export const SETSHARE      = 'user/share'
+export const USER_UID      = 'user/uid'
+
+export const QUESTION_INIT     = 'question/init'
+export const QUESTION_SETCOUNT = 'question/set/count'
+export const QUESTION_PLAY     = 'question/play'
+
+
+
+export const SET_BANNER = 'banner/set'
